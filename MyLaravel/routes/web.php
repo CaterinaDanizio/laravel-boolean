632356ce6846a62e6e3df8207d1b51b07392093e
Caterina <danizio@outlook.it>
2021-01-22 15:44:25 +0100

    Esercizio parte inglese

--- MyLaravel/routes/web.php
+++ MyLaravel/routes/web.php
@@ -16,3 +16,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Salutare tutti gli utenti in inglese (statico, no variabili)
+
+Route::get('/helloAll', function () {
+    return view('home');
+});
+
+// Salutare utente per nome (variabile) in inglese
+Route::get('/helloYou', function () {
+    $name = 'Caterina';
+    return view('home2', compact('name'));
+});
+
+// Salutare utente per nome (stessa variabile, stessa view, ma nome diverso) in inglese
+
+Route::get('/helloYou2', function () {
+    $name = 'Luca';
+    return view('home2', compact('name'));
+});
